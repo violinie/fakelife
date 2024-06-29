9c096c146106b51bb0f426b44d836ae2e59bb952
Violinie <hancacopakova@gmail.com>
2024-06-29 22:40:05 +0200

    feat(posts): update data

--- data/posts.ts
+++ data/posts.ts
@@ -10,35 +10,35 @@ export interface Post {
       id: 1,
       author_id: 1,
       coverImg: "https://source.unsplash.com/random/403x403?monstera",
-      description: "xy",
+      description: "monstera",
       date: "září 13, 2019"
     },
     {
       id: 2,
       author_id: 1,
       coverImg: "https://source.unsplash.com/random/403x403?scindapsus",
-      description: "xy",
+      description: "scindapsus",
       date: "říjen 14, 2019"
     },
     {
       id: 3,
       author_id: 1,
       coverImg: "https://source.unsplash.com/random/403x403?syngonium",
-      description: "xy",
+      description: "syngonium",
       date: "listopad 2, 2019"
     },
     {
       id: 4,
       author_id: 2,
       coverImg: "https://source.unsplash.com/random/403x403?cards",
-      description: "xy",
+      description: "cards",
       date: "září 2, 2019"
     },
     {
       id: 5,
       author_id: 2,
       coverImg: "https://source.unsplash.com/random/403x403?memory",
-      description: "xy",
+      description: "memory",
       date: "říjen 15, 2019"
     },
   ]
